7ad69995c0b02e149b7001a9cb5667c84ec8c997
Alex Messina <atm1984@gmail.com>
2014-08-12 03:56:40 -1100

    Got the  ellipses to look correct
    There was a problem with the input arrays to the cov() step so the
    covariance matrix was just nonsense

--- Code/PlotDrifters_Gridcell_Ellipses.py
+++ Code/PlotDrifters_Gridcell_Ellipses.py
@@ -4,7 +4,7 @@ Created on Fri Aug 01 07:48:32 2014
 
 @author: Alex
 """
-
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -49,13 +49,13 @@ create_new = False
 if create_new==True:
     #AllPoints = speed_and_bearing(tracklist,gridshape=grid,launchzoneshape=launch)
     AllPoints = speed_and_bearing_to_file(dirs,tracklist,gridshape=grid,launchzoneshape=launch)
-elif create_new==True:
+elif create_new==False:
     AllPoints = pd.DataFrame.from_csv(datadir+'AllPoints.csv') 
 
 
 ## Select deployments, cut to deployment time
 endmembers={"wind":range(9,13),"tide":range(13,21),"wave":range(21,31),"all":range(1,31)} ## range are non inclusive
-by_dep='wave'## Set to None if you want to show all the data
+by_dep='tide'## Set to None if you want to show all the data
 if by_dep!=None:
     ## Open Spreadsheet of deployment data
     XL = pd.ExcelFile(datadir+'Drifter deployment checklist.xlsx')
@@ -85,8 +85,8 @@ Map=Drifter_Map(dirs,MapExtent='Local',showLatLonGrid=False,showBackgroundImage=
 
 #### Analyze by Gridcell
 gridcount=len(grid.shapes()) ##establish grid count
-Ells = []
 AllGridValues=pd.DataFrame()
+
 
 for shape in grid.shapes():
     ## get center of shape by finding the mean lat and lon of all points in shapefile polygon
@@ -94,37 +94,38 @@ for shape in grid.shapes():
     for point in shape.points:
         grid_lons.append(point[0])
         grid_lats.append(point[1])
-    x0,y0 = Map(min(grid_lons),min(grid_lats))[0], Map(min(grid_lons),min(grid_lats))[1]
-    x1,y1 = Map(max(grid_lons),max(grid_lats))[0], Map(max(grid_lons),max(grid_lats))[1]
-    #print x0,y0,x1,y1
-    grid_bbox = Bbox([[x0,y0],[x1,y1]])
-    randColor= np.random.rand(3,1)
-    #Map.plot([x0,x1],[y0,y1],latlon=False,color=randColor)
-    
     grid_lon, grid_lat = np.mean(grid_lons),np.mean(grid_lats) ## in decimal degrees
-    grid_lon, grid_lat = Map(grid_lon,grid_lat,inverse=False)
+    grid_lon, grid_lat = Map(grid_lon,grid_lat,inverse=False) ## in projected degrees
+    
     ## get points from the Gridcell
     gridpoints = AllPoints[AllPoints['Gridcell']==gridcount].dropna()
-    if len(gridpoints) >= 2:
-        uv= gridpoints[['easting','northing']].values
-        u, v = gridpoints['easting'].values, gridpoints['northing'].values
+    print 'Analyzing Gridcell '+str(gridcount)+', '+str(len(gridpoints))+' points'
+    if len(gridpoints) >= 4:
+        rotation = 0 #the angle to rotate the axes counter-clockwise to bring the positive alongshore component into the strike of the coast and
+        #positive onshore component 90 degrees clockwise from the positive alongshore orientation if cross-shore and alongshore components are 
+        #desired instead of east-west/north-south (if E-W/N-S is desired, use a rotation angle = 0).
+        gridpoints['bearing'] = gridpoints['bearing']+rotation
+        u = (sin(gridpoints['bearing']*pi/180)*gridpoints['speed m/s']).values # E-W or cross-shore velocity
+        v = (cos(gridpoints['bearing']*pi/180)*gridpoints['speed m/s']).values # N-S or alongshore velocity
+        
         u_avg, v_avg = u.mean(), v.mean()
         ud, vd = u-u_avg, v-v_avg
         
-        cov_uv = np.cov(uv.T) ##transpose array to input to cov
+        cov_uv = np.cov(np.array([ud,vd])) ##transpose array to input to cov
         uv = cov_uv[0,1]
         uu = cov_uv[0,0]
         vv = cov_uv[1,1]
         Angle = 0.5*math.atan2(2*uv,uu-vv)
         
-        ua = ud*cos(Angle)+vd*sin(Angle)
+        ua = ud*cos(Angle)+vd*sin(Angle) # component in principal axis direction
         va = vd*cos(Angle)-ud*sin(Angle)
         
-        cov_uva = np.cov([ua,va])
+        cov_uva = np.cov(np.array([ua,va]))
         uva = cov_uva[0,1]
         uua = cov_uva[0,0]
         vva = cov_uva[1,1]
         
+        #Plot ellipse and principal axis
         ra, rb = sqrt(uua), sqrt(vva)
         ellipseX = ra*cos(frange(0,2*pi,2*pi/300))*cos(Angle)-sin(Angle)*rb*sin(frange(0,2*pi,2*pi/300))
         ellipseY = ra*cos(frange(0,2*pi,2*pi/300))*sin(Angle)+cos(Angle)*rb*sin(frange(0,2*pi,2*pi/300))
@@ -134,37 +135,39 @@ for shape in grid.shapes():
         Minor_x1,Minor_x2 = [-sqrt(vva)*cos(pi/2-Angle), sqrt(vva)*cos(pi/2-Angle)]
         Minor_y1,Minor_y2 = [sqrt(vva)*sin(pi/2-Angle),-sqrt(vva)*sin(pi/2-Angle)]
         
-#        
-#        plt.plot(ellipseX,ellipseY,c='k')
-#        plt.plot([Major_x1,Major_x2],[Major_y1,Major_y2],c='r')
-#        plt.plot([Minor_x1,Minor_x2],[Minor_y1,Minor_y2],c='g')
-#        plt.axis('equal')        
-        
-        XY = np.array([grid_lon,grid_lat])
-        U =((Major_x1-Major_x2)**2.0 + (Major_y1-Major_y2)**2.0)**0.5
-        V =((Minor_x1-Minor_x2)**2.0 + (Minor_y1-Minor_y2)**2.0)**0.5
+      
         # ellipses ...
         #e = Map.ax.add_artist(Ellipse(xy=XY,width=U,height=V,label=str(gridcount),color='r',fill=False,lw=1))  
-        scale_factor = 8
-        Map.plot([grid_lon+Major_x1*scale_factor,grid_lon+Major_x2*scale_factor],[grid_lat+Major_y1*scale_factor,grid_lat+Major_y2*scale_factor],c='r',lw=1)
-        Map.plot([grid_lon+Minor_x1*scale_factor,grid_lon+Minor_x2*scale_factor],[grid_lat+Minor_y1*scale_factor,grid_lat+Minor_y2*scale_factor],c='r',lw=1)
-        Map.plot(grid_lon+ellipseX*scale_factor,grid_lat+ellipseY*scale_factor,c='r',lw=1)
-        #plt.text(grid_lon,grid_lat,str(gridcount))
-        ##Raw point data (easting/northing)
-        #Map.plot(grid_lon+ud,grid_lat+vd,'og')
+        scale_factor = 500
+        ellipse_color, cNorm = mpl.cm.rainbow(len(gridpoints)), mpl.colors.Normalize(vmin=0,vmax=200) ## =Num of observations
+        #Major axis
+        Map.plot([grid_lon+Major_x1*scale_factor,grid_lon+Major_x2*scale_factor],[grid_lat+Major_y1*scale_factor,grid_lat+Major_y2*scale_factor],c=ellipse_color,lw=1)
+        #Minor axis        
+        Map.plot([grid_lon+Minor_x1*scale_factor,grid_lon+Minor_x2*scale_factor],[grid_lat+Minor_y1*scale_factor,grid_lat+Minor_y2*scale_factor],c=ellipse_color,lw=1)
+        #Ellipse        
+        Map.plot(grid_lon+ellipseX*scale_factor,grid_lat+ellipseY*scale_factor,c=ellipse_color,lw=1)
         
-        u=gridpoints['speed m/s'].values    
+        
+        ##Raw point data (easting/northing)
+        scale_factor = 100
+        #Map.plot(grid_lon+u*scale_factor,grid_lat+v*scale_factor,'og')
+        #Map.plot(grid_lon,grid_lat,'or')
+    
+        qHndl = Map.quiver(grid_lon,grid_lat,u_avg*scale_factor,v_avg*scale_factor,color=ellipse_color)
+        
+        #Mean speed/direction
+        M=gridpoints['speed m/s'].values    
         D=gridpoints['bearing'].values
     
         ve = 0.0 # define east component of wind speed
         vn = 0.0 # define north component of wind speed
         D = D * math.pi / 180.0 # convert wind direction degrees to radians
-        for i in range(0, len(u)):
-            ve = ve + u[i] * math.sin(D[i]) # calculate sum east speed components
-            vn = vn + u[i] * math.cos(D[i]) # calculate sum north speed components
+        for i in range(0, len(M)):
+            ve = ve + M[i] * math.sin(D[i]) # calculate sum east speed components
+            vn = vn + M[i] * math.cos(D[i]) # calculate sum north speed components
         ve = - ve / len(u) # determine average east speed component
         vn = - vn / len(u) # determine average north speed component
-        uv = math.sqrt(ve * ve + vn * vn) # calculate wind speed vector magnitude
+        u_v = math.sqrt(ve * ve + vn * vn) # calculate wind speed vector magnitude
         #Calculate wind speed vector direction
         vdir = scipy.arctan2(ve, vn)
         vdir = vdir * 180.0 / math.pi # Convert radians to degrees
@@ -175,7 +178,7 @@ for shape in grid.shapes():
                 Dv = vdir - 180
             else:
                 Dv = vdir
-        GridMean_speed = uv
+        GridMean_speed = u_v
         GridMean_bearing=Dv
         GridNumObs = len(gridpoints)
         print 'Mean speed of '+str(GridNumObs)+' points in Gridcell '+str(gridcount)+' = '+'%.2f'%GridMean_speed+' m/s, bearing '+'%.2f'%GridMean_bearing
@@ -185,10 +188,18 @@ for shape in grid.shapes():
 
     gridcount-=1 # count down from total length of grid by 1  
 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+divider = make_axes_locatable(Map.ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+cbar = mpl.colorbar.ColorbarBase(cax,cmap=plt.cm.rainbow,norm=cNorm,orientation='vertical')
+cbar.set_label('# observations')
+
+
+
 #### Plot arrows by speed
 ## Plot dirction arrows (lon and lat of where the point is, U and V of arrow vector (use sin and cos of the dirction in radians), color by speed)    
 from DrifterDataAnalysisTools import plot_arrows_by_speed
-plot_arrows_by_speed(Map,AllPoints)
+#plot_arrows_by_speed(Map,AllPoints)
 
 
 ## Plot mean velocity 
